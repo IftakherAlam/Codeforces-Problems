92e6096a89de058e3348f3d1199cb66964193193
IftakherAlam01 <iftakheralam.ewu@gmail.com>
2024-01-28 21:11:10 +0600

    Create 1925A.cpp

---
+++ 1925A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long double
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define fl(i,n) for(int i=0;i<n;i++)
+#define rl(i,m,n) for(int i=n-1;i>=m;i--)
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define pi 3.141592653589793238
+#define vr(v) v.begin(),v.end()
+#define rv(v) v.end(),v.begin()
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Wolf cout.tie(NULL);
+
+int main ()
+{
+   Code By Wolf
+   int t;
+   cin>>t;
+   while(t--)
+   {
+      int n,k;
+      cin>>n>>k;
+
+      string ans="";
+
+      for(int i=0;i<n;i++)
+      {
+         for(int j=0;j<k;j++)
+         {
+            ans+=(char)('a'+j);
+         }
+      }
+      cout<<ans<<endl;
+   }
+}
