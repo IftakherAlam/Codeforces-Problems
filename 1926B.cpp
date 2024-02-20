eefdd9296b0d147b000b35ea3d61a326ad40dca3
IftakherAlam01 <iftakheralam.ewu@gmail.com>
2024-02-20 20:01:54 +0600

    Create 1926B.cpp

---
+++ 1926B.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define lld long double
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define fl(i,n) for(int i=0;i<n;i++)
+#define rl(i,m,n) for(int i=n-1;i>=m;i--)
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define pi 3.141592653589793238
+#define vr(v) v.begin(),v.end()
+#define rv(v) v.end(),v.begin()
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Wolf cout.tie(NULL);
+int main()
+{
+   Code By Wolf
+   int t;
+   cin>>t;
+   while(t--)
+   {
+      int n;
+      cin>>n;
+     set<int>sizes;
+
+     for(int i=0;i<n;i++)
+     {
+      string s;
+      cin>>s;
+      int sz=0;
+      for(auto &i : s) sz+=(i=='1');
+      if (sz) sizes.insert(sz);
+     }
+
+   cout<<(sizes.size()>1 ? "TRIANGLE":"SQUARE")<<"\n";
+      
+   }
+   
+
+} 
