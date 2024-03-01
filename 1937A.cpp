281149e5ff3520ae75987fab03cfca8d1b8510a4
IftakherAlam01 <iftakheralam.ewu@gmail.com>
2024-03-01 23:10:14 +0600

    Create 1937A.cpp

---
+++ 1937A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define lld long double
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define fl(i,n) for(int i=0;i<n;i++)
+#define rl(i,m,n) for(int i=n-1;i>=m;i--)
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define pi 3.141592653589793238
+#define vr(v) v.begin(),v.end()
+#define rv(v) v.end(),v.begin()
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Wolf cout.tie(NULL);
+
+int main()
+{
+   Code By Wolf
+   
+     int t;
+     cin>>t;
+     while(t--)
+     {
+      int n;
+      cin>>n;
+
+      int ans = pow(2,floor(log2(n)));
+
+      cout<<setprecision(100)<<ans<<endl;
+     } 
+    return 0;
+} 
