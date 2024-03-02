272a64fdc5255bd11da2aa3016793713941d2542
IftakherAlam01 <iftakheralam.ewu@gmail.com>
2024-03-02 22:25:27 +0600

    Create 1934A.cpp

---
+++ 1934A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define lld long double
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define fl(i,n) for(int i=0;i<n;i++)
+#define rl(i,m,n) for(int i=n-1;i>=m;i--)
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define pi 3.141592653589793238
+#define vr(v) v.begin(),v.end()
+#define rv(v) v.end(),v.begin()
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Wolf cout.tie(NULL);
+
+int main()
+{
+   Code By Wolf
+  #ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+  #endif
+     int tt; cin >> tt;
+ 
+    while(tt--) {
+        int n; cin >> n;
+        vector<int>a(n);
+        for(auto &i : a) cin >> i;
+ 
+        sort(a.begin(), a.end());
+ 
+        cout << abs(a[0] - a[n - 1]) + abs(a[n - 1] - a[1]) + abs(a[1] - a[n - 2]) + abs(a[n - 2] - a[0]) << "\n";
+    }
+} 
