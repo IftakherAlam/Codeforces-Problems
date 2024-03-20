aac43d876bf3ec443523ac1b0f76edd66fcb059b
IftakherAlam01 <iftakheralam.ewu@gmail.com>
2024-03-20 22:00:07 +0600

    Create 1945A.cpp

---
+++ Codeforces/1945A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define lld long double
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define fl(i,n) for(int i=0;i<n;i++)
+#define rl(i,m,n) for(int i=n-1;i>=m;i--)
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define pi 3.141592653589793238
+#define vr(v) v.begin(),v.end()
+#define rv(v) v.end(),v.begin()
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Wolf cout.tie(NULL);
+
+int dx[4]={-1,1,-1,1},dy[4]={-1,-1,1,1};
+
+int main()
+{
+   Code By Wolf
+  #ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+  #endif
+    
+      int t;
+      cin>>t;
+      while(t--)
+      {
+        int a,b,c;
+        cin>>a>>b>>c;
+        ll ans =a;
+        ans+=b/3;
+        b%=3;
+        if(b>0 && 3-b>c)
+        {
+          cout<<-1<<endl;
+          continue;
+          
+        }
+
+        if(b>0)
+        {
+          ans++;
+          c-=3-b;
+        }
+        b=0;
+        ans+=(c+2)/3;
+        cout<<ans<<endl;
+      }
+  }
